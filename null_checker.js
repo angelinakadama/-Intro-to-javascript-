736f46072aa8263aa935984416eedd569d545f01
angelinakadama <angelinakadama32@gmail.com>
2019-06-16 18:45:13 -0700

    null.js

---
+++ null_checker.js
@@ -0,0 +1,12 @@
+for (var i = 1; i < m<10; i++)//for the loop to count ten times
+counter.prompt("input.number")
+console.log(1);{
+	//Displays a number greater than 0
+}	
+console.log(-1);{
+	//Displays a value less than 0
+}
+if ("counter>0");{
+
+}
+else if ("counter<0")
